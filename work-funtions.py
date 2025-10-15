ff30713035f493582f1da8347e2c9f5ac1a85165
Eric Gao <ericgaocan2@gmail.com>
2025-10-15 11:43:06 -0700

    calculate average

---
+++ work-funtions.py
@@ -0,0 +1,9 @@
+def getAverage():
+    num_one = int(input("Number 1: "))
+    num_two = int(input("Number 2: "))
+    num_three = int(input("Number 3: "))
+    average = (num_one + num_two + num_three) / 3
+    print(f"The average of the three numbers is: {average}")
+
+
+getAverage()
